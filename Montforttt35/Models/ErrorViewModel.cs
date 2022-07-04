c2a3db5e3a98f4ac18623f2f7f13cd57faa45789
Elouan DUVAL <elouan.duval35@gmail.com>
2022-07-04 21:59:55 +0200

    Ajoutez des fichiers projet.

---
+++ Montforttt35/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Montforttt35.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
